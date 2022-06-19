eb3c1b7d8c477ed86f0da401a2553769ae1588e5
joduckyong <dyddl7609@gmail.com>
2022-06-19 17:32:17 +0900

    Sandbox - 분석결과 조회

--- src/main/java/kr/or/lx/sandbox/result/controller/AnalysisResultController.java
+++ src/main/java/kr/or/lx/sandbox/result/controller/AnalysisResultController.java
@@ -1,11 +1,20 @@
 package kr.or.lx.sandbox.result.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import kr.or.lx.common.ApiService;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -13,6 +22,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class AnalysisResultController {
+	
+    @Value("${sandbox.api.url}")
+    private String sandboxApiUrl;	
+    
+	@Autowired
+	private ApiService<?> apiService;
 	
 	/**
      * 데이터 분석 결과 조회
@@ -26,6 +41,21 @@ public class AnalysisResultController {
 		return "sandbox/result/analysisResult/list";
 	}
 	
-	
+	/**
+     * 샌드박스 분석결과 
+     * @return
+     */	
+	@ResponseBody
+	@PostMapping("{apiId}")
+	public Object analysisResultApi(@RequestBody Map<String, Object> param, ModelMap model) throws Exception{
+		log.info("analysisResultApi");
+		
+		String url = sandboxApiUrl+param.get("url");
+		
+		ResponseEntity<?> responseEntity = apiService.post(url, param);
+		Object object = responseEntity.getBody();
+		
+		return object;
+	}
 	
 }
